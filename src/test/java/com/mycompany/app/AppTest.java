9da72819df01f89ffb9577d43e29bd30018b5253
William Olson <willko747@gmail.com>
2022-09-27 09:00:04 -0500

    Initial Commit

---
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -0,0 +1,34 @@
+package com.mycompany.app;
+
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+
+        Assert.assertTrue(true);
+
+    }
+
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldDoSomethingElse()
+    {
+
+        Assert.assertEquals(2+2, 4);
+
+    }
+}
